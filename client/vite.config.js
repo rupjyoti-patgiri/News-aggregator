41c1d7838d93a321fe40209ee5e6752a7a76d081
rupjyoti-patgiri <rupjyotipatgiri2004@gmail.com>
2025-06-11 13:15:13 +0530

    Fix Tailwind config and setup

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,10 +1,11 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import tailwindcss from '@tailwindcss/vite'
+// import tailwindcss from 'tailwindcss'
+// import postcss from 'postcss'     
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(),
-    tailwindcss(),
+    // tailwindcss(),
   ],
 })
